a5aaf8c14b1f6a8c664462481043c99152890902
Mike Vatcher <mike_vatcher@yahoo.com>
2021-10-29 13:50:31 -0230

    1st clean up commit

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAfXW-pxn_icNG-uIsdyRQr0ttMukYt92I",
+  authDomain: "twitter-clone-49e8e.firebaseapp.com",
+  projectId: "twitter-clone-49e8e",
+  storageBucket: "twitter-clone-49e8e.appspot.com",
+  messagingSenderId: "809290059000",
+  appId: "1:809290059000:web:e7bdd9966095712b073f0d",
+  measurementId: "G-38X3Z0GKFS"
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
