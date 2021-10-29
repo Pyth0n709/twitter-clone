f77ee6087a137036bdccf1482042d575350f490d
Mike Vatcher <mike_vatcher@yahoo.com>
2021-10-29 14:09:13 -0230

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar/Sidebar';
 
 function App() {
   return (
     //BEM naming convention
     <div className="app">
         {/* <Sidebar /> */}
-
+        <p>We are working on a twitter clone!!</p>
         {/* <Feed /> */}
 
         {/* <Widgets /> */}
